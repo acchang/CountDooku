2269e3120e8fb9304bf0f81266d9d6e404d7583a
Andrew Chang <acchang4@gmail.com>
2022-06-12 16:23:11 -0400

    mongo info

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express();
+
+const dotenv = require('dotenv') // .env file
+dotenv.config() // using .env
+const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority`;
+
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
