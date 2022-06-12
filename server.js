df2b45c0f29ae4da1226c5f9a81fa06b524cf2fc
Andrew Chang <acchang4@gmail.com>
2022-06-12 17:01:20 -0400

    render views

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+
 
 const dotenv = require('dotenv') // .env file
 dotenv.config() // using .env
 const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.iyram.mongodb.net/?retryWrites=true&w=majority`;
-
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
@@ -16,6 +15,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     const db = client.db('DookuLog')
     const BMcollection = db.collection('events')
 
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.set('view engine', 'ejs')
 
     app.get('/', (req, res) => {
       res.sendFile(__dirname + '/index.html')
@@ -32,8 +33,8 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     app.listen(3000, function() {
       console.log('listening on 3000')
     })
-  })
 
-console.log('The Dooku is Strong With This One') 
+
+  })
 
 
