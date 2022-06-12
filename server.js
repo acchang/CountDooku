b951e864c82e6a44aa2cc43672624e8e64ba4589
Andrew Chang <acchang4@gmail.com>
2022-06-12 15:46:12 -0400

    at mongo

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser= require('body-parser')
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 console.log('The Dooku is Strong With This One')
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
+  })
+
+app.post('/log', (req, res) => {
+  console.log(req.body)
   })
 
 app.listen(3000, function() {
