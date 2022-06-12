b78c3df32d4d7e498a01af22f562f34ff613392a
Andrew Chang <acchang4@gmail.com>
2022-06-12 16:52:50 -0400

    rendering

--- server.js
+++ server.js
@@ -3,31 +3,37 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const dotenv = require('dotenv') // .env file
 dotenv.config() // using .env
-const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.jqbcy.mongodb.net/?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PW}@cluster0.iyram.mongodb.net/?retryWrites=true&w=majority`;
 
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
-    const db = client.db('star-wars-quotes')
-    const quotesCollection = db.collection('quotes')
+    const db = client.db('DookuLog')
+    const BMcollection = db.collection('events')
 
-    
 
-app.use(bodyParser.urlencoded({ extended: true }))
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html')
+    })
 
-console.log('The Dooku is Strong With This One')
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+    app.post('/log', (req, res) => {
+      BMcollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+  
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
   })
 
-app.post('/log', (req, res) => {
-  console.log(req.body)
-  })
+console.log('The Dooku is Strong With This One') 
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+
